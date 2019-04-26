d6b0509ddd7cfa6546d61fe2e412424cc0140f86
Nic Wainwright <wainwrightnic@gmail.com>
2019-04-25 20:52:40 -0400

    basic first window up and running. need to figure out how to save value from an Entry item for email input[#1]

--- gui.py
+++ gui.py
@@ -6,3 +6,22 @@ Created on Thu Apr 25 20:15:05 2019
 """
 
 import tkinter as tk
+
+def retrieve_input():
+    inputValue = emailBox.get("1.0","end-1c")
+    print(inputValue)
+
+root = Tk()
+root.title('Final Project')
+content = tk.Frame(root).grid(column=0, row=0)
+root.geometry('400x200')
+instruction_lbl = tk.Label(content, text='Please enter your email.').grid(column=2, row=1)
+emailBox= tk.Entry(content).grid(column=2, row=2)
+submit_btn = tk.Button(content, text='Submit Email', command=lambda: retrieve_input()).grid(column=2, row=3)
+
+root.mainloop()
+
+
+
+
+
