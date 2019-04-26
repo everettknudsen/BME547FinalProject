5fffdfd0aadaca3b4f7cab88f498dec117ac80aa
Nic Wainwright <wainwrightnic@gmail.com>
2019-04-26 12:58:52 -0400

    [#1] got a flow from window to another window login to upload/download to upload options. using a destroy definition for each window

--- gui.py
+++ gui.py
@@ -7,43 +7,68 @@ Created on Thu Apr 25 20:15:05 2019
 
 import tkinter as tk
 
+local_url = 'http://127.0.0.1:5000'
 
-def retrieve_input(entryBox):
+def retrieve_email(lbl, entryBox, btn):
     emailBox = entryBox
     global email
     email = emailBox.get()
-    global root
-    root.destroy()
-    print(email)
+    # requests.post(local_url+'/api/login', json=email)
+    destroyEmail(lbl, entryBox, btn)
+    getLoadType()
 
 
 def getEmail():
-    root.title('Final Project')
+    root.title('Login Screen')
     content_email = tk.Frame(root).grid(column=0, row=0)
     root.geometry('400x200')
     instruction_lbl = tk.Label(content_email, text='Please enter your email.')
     instruction_lbl.grid(column=1, row=1)
     emailBox= tk.Entry(content_email)
     emailBox.grid(column=2, row=2)
-    submit_btn = tk.Button(content_email, text='Submit Email', command=lambda: retrieve_input(emailBox))
+    submit_btn = tk.Button(content_email, text='Submit Email', command=lambda: retrieve_email(instruction_lbl, emailBox, submit_btn))
     submit_btn.grid(column=2, row=3)
     root.mainloop()
 
 
+def destroyEmail(lbl, box, btn):
+    lbl.destroy()
+    box.destroy()
+    btn.destroy()
+
+
 def getLoadType():
-    root.title('Final Project')
+    root.title('Load Type')
     content_loadType = tk.Frame(root).grid(column=0, row=0)
     root.geometry('400x200')
     instruction_lbl = tk.Label(content_loadType, text='Would you like to upload a photo(s) or download?')
     instruction_lbl.grid(column=2, row=1)
-    upload_btn = tk.Button(content_loadType, text='', command="buttonpressed")
+    upload_btn = tk.Button(content_loadType, text='Upload', command=lambda: uploadPressed(instruction_lbl, upload_btn, download_btn))
     upload_btn.grid(column=2, row=3)
+    download_btn = tk.Button(content_loadType, text='Download', command="buttonpressed")
+    download_btn.grid(column=3, row=3)
     root.mainloop()
 
 
-root = Tk()
+def destroyLoadType(lbl, up, down):
+    lbl.destroy()
+    up.destroy()
+    down.destroy()
+    
+
+
+def uploadPressed(lbl, btn1, btn2):
+    destroyLoadType(lbl, btn1, btn2)
+    root.title('Uploading')
+    content_loadType = tk.Frame(root).grid(column=0, row=0)
+    root.geometry('400x200')
+    instruction_lbl = tk.Label(content_loadType, text='Uploading a photo')
+    instruction_lbl.grid(column=2, row=1)
+
+root = tk.Tk()
 email = ''
 getEmail()
+
 
 
 
