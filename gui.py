4d7c18b41f6e6deddb577839381405d11baced45
Nic Wainwright <wainwrightnic@gmail.com>
2019-04-26 12:20:29 -0400

    [#1] movedd gui interface for screen one - email - into a definition

--- gui.py
+++ gui.py
@@ -7,23 +7,43 @@ Created on Thu Apr 25 20:15:05 2019
 
 import tkinter as tk
 
-def retrieve_input():
-    inputValue = emailBox.get()
-    print(inputValue)
+
+def retrieve_input(entryBox):
+    emailBox = entryBox
+    global email
+    email = emailBox.get()
+    global root
+    root.destroy()
+    print(email)
+
+
+def getEmail():
+    root.title('Final Project')
+    content_email = tk.Frame(root).grid(column=0, row=0)
+    root.geometry('400x200')
+    instruction_lbl = tk.Label(content_email, text='Please enter your email.')
+    instruction_lbl.grid(column=1, row=1)
+    emailBox= tk.Entry(content_email)
+    emailBox.grid(column=2, row=2)
+    submit_btn = tk.Button(content_email, text='Submit Email', command=lambda: retrieve_input(emailBox))
+    submit_btn.grid(column=2, row=3)
+    root.mainloop()
+
+
+def getLoadType():
+    root.title('Final Project')
+    content_loadType = tk.Frame(root).grid(column=0, row=0)
+    root.geometry('400x200')
+    instruction_lbl = tk.Label(content_loadType, text='Would you like to upload a photo(s) or download?')
+    instruction_lbl.grid(column=2, row=1)
+    upload_btn = tk.Button(content_loadType, text='', command="buttonpressed")
+    upload_btn.grid(column=2, row=3)
+    root.mainloop()
+
 
 root = Tk()
-root.title('Final Project')
-content = tk.Frame(root).grid(column=0, row=0)
-root.geometry('400x200')
-instruction_lbl = tk.Label(content, text='Please enter your email.').grid(column=2, row=1)
-emailBox= tk.Entry(content)
-emailBox.grid(column=2, row=2)
-submit_btn = tk.Button(content, text='Submit Email', command=lambda: retrieve_input())
-submit_btn.grid(column=2, row=3)
-
-root.mainloop()
-
-
+email = ''
+getEmail()
 
 
 
