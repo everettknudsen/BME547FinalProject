ccf382ef85478951246813d5a47de68878d2d12f
Kendall Liang <kendall.liang@duke.edu>
2019-04-26 21:14:16 -0400

    Remove extraneous code [#2]

--- test_server.py
+++ test_server.py
@@ -16,20 +16,6 @@ im = data.moon()
 im_p_he = exposure.equalize_hist(im)
 
 
-@pytest.mark.parametrize('x, expected', [
-    ((im, 'moon', 'Histogram equalization'),
-     (im_p_he, 'moon_he'))
-])
-def test_process_image(x, expected):
-    # print('type of im: {}'.format(type(im)))
-    # print('type of im_p_he: {}'.format(type(im_p_he)))
-
-    from server import process_image
-    ans = process_image(x[0], x[1], x[2])
-    # print('type of ans0: {}'.format(type(ans[0])))
-    # print('type of ans1: {}'.format(type(ans[1])))
-    print(ans[1])
-    # assert ans[0].all == expected[0].all
-    assert ans[1] == expected[1]
+# test PIL, numpy type
 
 # add test for email error: import error
