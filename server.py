d3963dbd02332c276f0ba4c4db6b1af9c863bb18
Kendall Liang <kendall.liang@duke.edu>
2019-04-27 12:52:20 -0400

    Revised docstring format, populated skeleton [#5]

--- server.py
+++ server.py
@@ -8,7 +8,6 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-from skimage import exposure
 
 # global variables
 im = []
@@ -29,7 +28,9 @@ def post_login():
     """Queries user if email exists or
     creates new database for new user
 
-    :return:
+    Args:
+
+    Returns:
     """
     email = request.get_json()
     is_email(email)
@@ -39,39 +40,118 @@ def post_login():
     return jsonify(email)
 
 
-@app.route('/api/new_image', methods=['POST'])
-def post_new_image():
-    """
+@app.route('/api/<user>/post_new_image', methods=['POST'])
+def post_new_image(user):
+    """Uploads new image to database
 
-    :return:
+    Args:
+        user (str): user email (primary key)
+
+    Returns:
     """
-    im = request.get_json()
+    user = []
+    img = request.get_json()
     # decode image
 
     # for user in User.objects.raw({}):
     # assign image to user database
-    # return jsonify(im)
+    # return jsonify(img)
 
 
-@app.route('/api/image_list', methods=['GET'])
-def get_image_list():
-    """Obtains list of image titles from database
+@app.route('/api/<user>/get_image_list', methods=['GET'])
+def get_image_list(user):
+    """Obtains list of non-processed image titles from database
     for use in dropdown menu
 
-    :return: list of images
+    Args:
+        user (str): user email (primary key)
+
+    Returns:
+        image_list (list): list of unprocessed image  titles
     """
     # for user in User.objects.raw({}):
-    image_list = []
+    # image_dict = database['name']
+    image_list = pull_image_list(image_dict)
     return jsonify(image_list)
 
 
-@app.route('/api/choose_method', methods=['POST'])
-def post_choose_method():
-    """Choose processing type
+def pull_image_list(image_dict):
+    """MongoDB image dict for one user
 
-    :return:
+    Args:
+        image_dict (dict): dictionary of user images
+
+    Returns:
+        image_list (list): list of unprocessed image  titles
     """
-    im, im_name, method = request.get_json()
+    image_list = []
+    return image_list
+
+
+@app.route('/api/<user>/<img_name>/get_image', methods=['GET'])
+def get_image(user, img_name):
+    """Pulls up image name and data when chosen from dropdown menu
+
+    Args:
+        user (str): user email (primary key)
+        img_name (str): name of desired image
+
+    Returns:
+    """
+    img_data, metrics = pull_image(user, img_name)
+    return jsonify(img_name, img_data, metrics)
+
+
+def pull_image(img_name):
+    """Obtains info for one image from database
+
+    Args:
+        img_name (str): name of desired image
+
+    Returns:
+        img_data
+        metrics
+    """
+    # check database
+    # counts length of 'time' list for each '*_process' image
+    img_data = []
+    metrics = []
+    return img_data, metrics
+
+
+@app.route('/api/<user>/<process>/get_process_type', methods=['GET'])
+def get_process_count(user, process):
+    count = process_count(user, process)
+    return jsonify(count)
+
+
+def process_count(user, process):
+    """Determines how many times a user has applied a certain process
+
+    Args:
+        user (str): user email (primary key)
+        process (str): desired processing types
+
+    Returns:
+        count (int): number of times
+    """
+    # check database
+    # search for tag in string
+    # counts length of 'time' list for each '*_process' image
+    count = []
+    return count
+
+
+@app.route('/api/get_all_user_metrics/<user>', methods=['GET'])
+def get_all_user_metrics(user):
+    metrics = all_user_metrics(user)
+    return jsonify(metrics)
+
+
+def all_user_metrics(user):
+    # check database
+    # print list of timestamps/actions
+    return metrics
 
 
 class NotEmail(Exception):
