76b6b48e0d5d4105d2aa327d458bb513788ea995
everettknudsen <jek42@duke.edu>
2019-04-27 15:16:05 -0400

    Commit before rebase from master

--- metrics.py
+++ metrics.py
@@ -2,15 +2,15 @@ import mongoengine
 import datetime
 
 from images import Image
+from time_latency import TimeLatency
 
 
 class Metrics(mongoengine.EmbeddedDocument):
     file_name = mongoengine.ReferenceField(Image, primary_key=True)
-    date_registered = mongoengine.DateTimeField(datetime.datetime.now)
     processing_type = mongoengine.StringField(default='original',
                                               required=True)
-    processing_time = mongoengine.FloatField(required=True)
-    image_size = mongoengine.FloatField(required=True)
+    image_size = mongoengine.FloatFieldField(required=True)
+    time_latency = mongoengine.EmbeddedDocumentListField(required=True)
 
     meta = {
         'db_alias': 'core',
