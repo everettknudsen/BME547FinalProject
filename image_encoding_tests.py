b402b3add9335c4daa4335e001517ee9753d3fcf
everettknudsen <jek42@duke.edu>
2019-04-29 04:50:33 -0400

    Added docstrings for encode/decode functions

--- image_encoding_tests.py
+++ image_encoding_tests.py
@@ -9,22 +9,35 @@ from skimage import io as im
 
 
 def encode_image_as_b64(img_NParray):
+    """
+    A function which takes an image as a np array
+    and encdes it into a base64 string which can then
+    be passed to the server as a json.
+    :param img_NParray: An image as a np array.
+    :return img_str: A base64 encouded image string.
+    """
     img = Image.fromarray(img_NParray)
     buffer = io.BytesIO()
     img.save(buffer, format="PNG")
     img_bytes = buffer.getvalue()
     img_str = base64.b64encode(img_bytes).decode("utf-8")
-    # print(sys.getsizeof(img_str))
-    # print(img_str)
     return img_str
 
 
 def decode_image_from_b64(base64_img_str):
+    """
+    This function decodes a base64 image string back into
+    a PIL image usable with TKinter and GUI display.
+    :param base64_img_str: A base64 string containing encouded
+    image data.
+    :return decoded_img: PIL image file ready to use with GUI
+    or processing or download.
+    """
     tp = open("tp.png", "wb")
     tp.write(base64.b64decode(base64_img_str))
     tp.close()
     decoded_img = im.imread("tp.png")
-    return decoded_img  # type ndarray
+    return decoded_img
 
 
 if __name__ == '__main__':
@@ -32,4 +45,3 @@ if __name__ == '__main__':
     image_array = np.array(img)
     img_b64_string = encode_image_as_b64(image_array)
     img = decode_image_from_b64(img_b64_string)
-    # print(type(img))
