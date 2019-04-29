61418e833c9e7b12da8df1977bbcfaa8307ca687
Kendall Liang <kendall.liang@duke.edu>
2019-04-29 05:06:14 -0400

    Upload test_image_encoding_tests.py; clean up docstrings [#46]

--- test_gui.py
+++ test_gui.py
@@ -11,45 +11,33 @@ im0 = NumpytoPIL(im)
 
 
 def test_NumpytoPIL_0():
-    """First test of numpy to PIL conversion
+    """First test for numpy to PIL conversion
     tests that type of output is PIL image
-
-    Args:
-
-    Returns:
     """
-    ans = NumpytoPIL(im)
     # ensure is numpy and not PIL
     assert type(im) == np.ndarray
+    ans = NumpytoPIL(im)
 
     assert type(ans) == PIL.Image.Image
 
 
 def test_NumpytoPIL_1():
-    """Second test of numpy to PIL conversion
+    """Second test for numpy to PIL conversion
     tests that the image is the same before and after conversion
-
-    Args:
-
-    Returns:
     """
-    ans = NumpytoPIL(im)
-    # ensure is numpy and not PIL
+    # ensure im is numpy and not PIL
     assert type(im) == np.ndarray
 
+    ans = NumpytoPIL(im)
     assert ans == im0
 
 
 def test_PILtoNumpy():
-    """Test PIL to numpy conversion
+    """Test for PIL to numpy conversion
     tests that type of output is numpy ndarray
-
-    Args:
-
-    Returns:
     """
-    ans = PILtoNumpy(im0)
     # ensure im0 is PIL and not numpy
     assert type(im0) == PIL.Image.Image
+    ans = PILtoNumpy(im0)
 
     assert type(ans) == np.ndarray
