4883fe31bb64ea91b1cb03e58927438fa0903f23
Kendall Liang <kendall.liang@duke.edu>
2019-04-28 20:54:34 -0400

    Test PILtoNumpy, NumpytoPIL functions [#46]

---
+++ test_gui.py
@@ -0,0 +1,31 @@
+import pytest
+
+from skimage import exposure, data
+import PIL
+import numpy as np
+from gui import PILtoNumpy, NumpytoPIL
+
+global ans0
+im = data.moon()
+
+
+def test_PILtoNumpy():
+    from gui import PILtoNumpy
+
+    ans0 = PILtoNumpy(im)
+    return im
+    assert type(ans0) == np.ndarray
+
+
+def test_NumpytoPIL():
+    from gui import NumpytoPIL
+
+    ans = NumpytoPIL(im)
+    assert type(ans) == PIL.Image.Image
+'''
+
+@pytest.mark.parametrize('x, expected', [
+    (None, None),
+    (None, None),
+])
+'''
